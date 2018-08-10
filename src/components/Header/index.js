42dbc09de5c5fb9397749a9cc11134ff3b58d7b3
TijanaMilkic <tijana.milkic92@gmail.com>
2018-08-10 15:29:11 +0200

    Added Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function Header(props) {
+    return (
+        <div className="App-header">
+          <img src="http://lostseoulmovie.com/wp-content/uploads/2017/05/IMDB_Logo.png" className="App-logo" alt="logo" />
+          
+        </div>
+    );
+}
+
+export default Header;
